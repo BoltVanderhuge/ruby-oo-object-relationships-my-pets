263908cda582b17a16cea122044241f6b98f784c
john_wisneski <john.j.wisneski@gmail.com>
2020-12-10 18:10:57 -0500

    commit tree 5181d54e6115eeec88e07499da330773144bcc2e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
+require 'pry'
 class Dog
-  # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  @@all = []
+
+  def initialize (name_param, owner_param)
+  @name = name_param
+  @owner = owner_param
+  @mood = "nervous"
+  @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
+
+# odie=Dog.new("Odie")
+
+# binding.pry
